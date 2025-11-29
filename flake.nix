35ccef5d583a6d54581d7c2405b50aee8f2f26b2
zlepper <hansen13579@gmail.com>
2025-11-29 10:56:02 +0100

    Absolutely minimal hello world

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Example flake with a devShell";
+
+inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+outputs = { self, nixpkgs}:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      lib = pkgs.lib;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          libxcb
+          libGL
+          libxkbcommon
+          openssl
+          wayland
+          jetbrains.rust-rover
+
+             xorg.libXcursor
+                      xorg.libXrandr
+                      xorg.libXi
+                      xorg.libX11
+        ];
+        shellHook = ''
+            export LD_LIBRARY_PATH="${lib.makeLibraryPath buildInputs}:/run/opengl-driver/lib"
+        '';
+      };
+    };
+}
