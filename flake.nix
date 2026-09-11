dcde06ca20874202b5bf6acb001c9f0aaa740e5c
zlepper <hansen13579@gmail.com>
2026-09-11 12:20:50 +0200

    dev: add the licensing development toolchain

--- flake.nix
+++ flake.nix
@@ -1,37 +1,52 @@
 {
-  description = "Example flake with a devShell";
+  description = "Styrhous development environment";
 
-inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-outputs = { self, nixpkgs}:
+  outputs =
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-      lib = pkgs.lib;
-    in {
-      devShells.x86_64-linux.default = pkgs.mkShell rec {
-        buildInputs = with pkgs; [
-          libxcb
-          libGL
-          vulkan-loader
-          libxkbcommon
-          openssl
-          wayland
-          kind
-          kubectl
-          cargo-nextest
-          jetbrains.rust-rover
-          imagemagick
-          codex
-          gh
-             xorg.libXcursor
-                      xorg.libXrandr
-                      xorg.libXi
-                      xorg.libX11
-        ];
-        shellHook = ''
-            export LD_LIBRARY_PATH="${lib.makeLibraryPath buildInputs}:/run/opengl-driver/lib"
-        '';
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      playwrightBrowsers = pkgs.playwright-driver.browsers;
+      runtimeLibraries = with pkgs; [
+        libGL
+        libx11
+        libxcb
+        libxcursor
+        libxi
+        libxkbcommon
+        libxrandr
+        openssl
+        vulkan-loader
+        wayland
+      ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages =
+          (with pkgs; [
+            cargo-nextest
+            codex
+            dotnet-sdk_10
+            gh
+            imagemagick
+            jetbrains.rust-rover
+            kind
+            kubectl
+            nodejs_22
+            pulumi
+          ])
+          ++ runtimeLibraries;
+
+        env = {
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath runtimeLibraries}:/run/opengl-driver/lib";
+          PLAYWRIGHT_BROWSERS_PATH = "${playwrightBrowsers}";
+          PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
+        };
       };
     };
 }
